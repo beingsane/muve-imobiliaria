0041ce0c53bb41b80ae2d95d9a74462c5e394402
joaobenthin <joaopaulobenthin@hotmail.com>
2019-03-10 19:34:54 -0300

    backend

--- backend/app.js
+++
@@ -1,6 +0,0 @@
-/* importar as configurações do servidor */
-var app = require('./config/server')
-
-app.listen(3001, function() {
-    console.log('Servidor Express')
-})
--- backend/index.js
+++
@@ -1,2 +0,0 @@
-/* importar as configurações do servidor */
-const index = require('./config/server')
